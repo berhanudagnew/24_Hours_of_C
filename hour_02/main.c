90edcff19d7bbb5af2c9932a24351f9170b77ef9
berhanudagnew <berhanudagnew.github@gmail.com>
2023-01-13 16:04:30 +0200

    create h02 main.c

---
+++ hour_02/main.c
@@ -0,0 +1,14 @@
+/*This is ch02 exersice 1*/
+/*Write a C program that can print "It's fun to write my own program in C."*/
+
+/*header files*/
+#include "stdio.h"
+#include "stdlib.h"
+
+/*main function*/
+int main(){
+    /*adding question 3*/
+    printf("\nHello!, neighbor!\n");
+    printf("This is my first C program.\n");    
+    return 0;
+}
